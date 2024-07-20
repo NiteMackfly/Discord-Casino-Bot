5ee9b85165e47d5e09925378616e4e3c4ec9ece3
plawandos <pierrelawandos@gmail.com>
2024-07-20 15:21:26 +0300

    Update bot.py

--- discord/bot.py
+++ discord/bot.py
@@ -1,16 +1,31 @@
 import discord
 from discord.ext import commands
+import os
+import asyncio
 from modules.helpers import *
 
+intents = discord.Intents.default()
+intents.message_content = True  # Ensure to enable necessary intents
 
-client = commands.Bot(
-    command_prefix=PREFIX, owner_ids=OWNER_IDS, intents=discord.Intents.all()
-)
+client = commands.Bot(command_prefix=PREFIX, owner_ids=OWNER_IDS, intents=intents)
 
 client.remove_command("help")
 
-for filename in os.listdir(COG_FOLDER):
-    if filename.endswith(".py"):
-        client.load_extension(f"cogs.{filename[:-3]}")
+
+async def load_cogs():
+    for filename in os.listdir(COG_FOLDER):
+        if filename.endswith(".py"):
+            cog_name = f"cogs.{filename[:-3]}"
+            try:
+                await client.load_extension(cog_name)
+            except Exception as e:
+                print(f"Failed to load extension {cog_name}: {e}")
+
+
+@client.event
+async def on_ready():
+    await load_cogs()
+    print(f"Logged in as {client.user}")
+
 
 client.run(TOKEN)
